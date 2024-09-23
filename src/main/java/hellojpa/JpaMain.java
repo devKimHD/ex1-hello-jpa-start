8f7771a8f0d4d32ae13c027addc9f44eddad8d73
KIMHYUNGDONG <khd30425@gmail.com>
2024-09-23 22:25:11 +0900

    jpa 기본키 전략

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -25,13 +25,47 @@ public class JpaMain {
 //            System.out.println("findMember"+findMember.getName());
 //            System.out.println("findMember"+findMember.getId());
 //            findMember.setName("nameUp");
-            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(0)
-                    .setMaxResults(5)
-                    .getResultList();
-            for (Member member : resultList) {
-                System.out.println("member "+member.getName());
-                }
+//            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(0)
+//                    .setMaxResults(5)
+//                    .getResultList();
+//            for (Member member : resultList) {
+//                System.out.println("member "+member.getName());
+//                }
+            //비영속
+//            Member member = new Member();
+//            member.setName("nameJpa");
+//            member.setId(101L);
+            //
+            //영속
+//            System.out.println("==Before==");
+//            em.persist(member);
+//            System.out.println("==After==");
+
+//            Member member1 = em.find(Member.class, 101L);
+//            Member member2 = em.find(Member.class, 101L);
+//            System.out.println(member1 == member2);
+//            Member memberA = new Member(150L, "A");
+//            Member memberB = new Member(160L, "B");
+//            em.persist(memberA);
+//            em.persist(memberB);
+//            System.out.println("=========================");
+//            Member member = em.find(Member.class, 150L);
+////            member.setName("A-Edit");
+//            member.setName("A-Edit2");
+////            em.detach(member);
+//            em.clear(); // 1차캐시 다 비움
+//            Member member2 = em.find(Member.class, 150L);
+//            System.out.println("=========================");
+
+//            Member member = new Member(200L, "newMem");
+//            em.persist(member);
+//            em.flush();
+//            System.out.println("tx before");
+            Member member = new Member();
+//            member.setId("usr_A");
+            member.setUsername("usr_nm1");
+            em.persist(member);
             tx.commit();
         }
         catch (Exception e)
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,16 +1,36 @@
 package hellojpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 //@Table(name="MEMBER")
-
+//@SequenceGenerator(name="member_seq_generator", sequenceName = "member_seq")
 public class Member {
+
+
     @Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    @Column(name = "name", nullable = false)
+    private String username;
+//    private Integer age;
+//    @Enumerated(EnumType.STRING)
+//    private RoleType roleType;
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date createdDate;
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date lastModifiedDate;
+//    @Lob
+//    private String description;
+//    @Transient
+//    private int tempData;
+    public Member()
+    {
+        //jpa 동적 객체 관리 위해 기본 생성자 추가
+    }
 
     public Long getId() {
         return id;
@@ -20,11 +40,11 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
